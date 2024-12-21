e71c070f47836194bb3fd6db773842787385d328
João Vitor <jvcbrandaomat@gmail.com>
2024-12-20 22:13:04 -0300

    Versão básica finalizada

--- Literalura/src/main/java/com/joaobrandao/Literalura/model/DadosIdiomas.java
+++
@@ -1,8 +0,0 @@
-//package com.joaobrandao.Literalura.model;
-//
-//import com.fasterxml.jackson.annotation.JsonAlias;
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//
-//@JsonIgnoreProperties(ignoreUnknown = true)
-//public record DadosIdiomas(@JsonAlias()) {
-//}
